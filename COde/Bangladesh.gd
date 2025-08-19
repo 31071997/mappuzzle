96db330cb26a6ac094128782a5aeefaf459c3aae
31071997 <zferdous35@gmail.com>
2025-08-20 02:00:18 +0600

    v22

--- COde/Bangladesh.gd
+++ COde/Bangladesh.gd
@@ -2,4 +2,3 @@ extends Node2D
 @onready var popup_panel: PopupPanel = $PopupPanel
 func _ready() -> void:
 	popup_panel.hide()
-	MainMenu.hide()
--- COde/main_menu.gd
+++ COde/main_menu.gd
@@ -1,16 +1,15 @@
 extends Control
 var url1 = 'res://Scene/Bangladesh.tscn'
 var count = 0
-@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
-
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file(url1)
+func mute_bgm():
+	var bus = AudioServer.get_bus_index("BUS")
+	AudioServer.set_bus_mute(bus, true)
 
-
-func _on_button_2_toggled(toggled_on: bool) -> void:
-	count += 1
-func _ready() -> void:
-	if(count%2 == 1):
-		audio_stream_player.stream_paused = true
-	else:
-		audio_stream_player.stream_paused = false
+func unmute_bgm():
+	var bus = AudioServer.get_bus_index("BUS")
+	AudioServer.set_bus_mute(bus, false)
+	
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	mute_bgm()
