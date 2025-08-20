e84f6fa249c90d0acd8c08e8e59b9f69002d7d4c
31071997 <zferdous35@gmail.com>
2025-08-21 00:58:02 +0600

    v23

--- COde/main_menu.gd
+++ COde/main_menu.gd
@@ -1,15 +1,20 @@
 extends Control
 var url1 = 'res://Scene/Bangladesh.tscn'
 var count = 0
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file(url1)
-func mute_bgm():
-	var bus = AudioServer.get_bus_index("BUS")
-	AudioServer.set_bus_mute(bus, true)
-
-func unmute_bgm():
-	var bus = AudioServer.get_bus_index("BUS")
-	AudioServer.set_bus_mute(bus, false)
-	
+func pauseBGM():
+	for player in get_tree().get_nodes_in_group("BGM"):
+		player.stream_paused = true
+func playBGM():
+	for player in get_tree().get_nodes_in_group("BGM"):
+		player.stream_paused = false
+func _ready() -> void:
+	audio_stream_player_2d.play()
 func _on_check_button_toggled(toggled_on: bool) -> void:
-	mute_bgm()
+	if toggled_on:
+		pauseBGM()
+	else:
+		playBGM() 
