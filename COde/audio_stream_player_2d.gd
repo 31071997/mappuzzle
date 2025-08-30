4a6fd8d878c869800ee2374eb9151b71a707aaec
31071997 <zferdous35@gmail.com>
2025-08-30 18:02:20 +0600

    v25

--- COde/audio_stream_player_2d.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
-'''func playAudio():
-	get_tree().paused = false
-func pauseAudio():
-	get_tree().paused = true'''
