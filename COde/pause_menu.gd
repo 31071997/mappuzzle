cbee22f9ccf24b430b26cd096b25f050c38a5f96
31071997 <zferdous35@gmail.com>
2025-09-10 18:23:46 +0600

    v30

--- COde/pause_menu.gd
+++ COde/pause_menu.gd
@@ -4,7 +4,22 @@ extends Control
 
 func _ready() -> void:
 	var popup = menu_button.get_popup()
+	popup.add_item("Play", 0)
+	popup.add_item("Quit", 1)
+	popup.add_item("Restart", 2)
+	popup.add_item("Load", 3)
 
+	popup.id_pressed.connect(_on_menu_item_selected)
+func _on_menu_item_selected(id: int) -> void:
+	match id:
+		0:
+			get_tree().paused = false
+		1:
+			print(id)
+		2:
+			print(id)
+		3:
+			print(id)
 
 func _on_menu_button_pressed() -> void:
 	get_tree().paused = true
