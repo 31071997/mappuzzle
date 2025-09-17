4e260e4341986b55d811428de788711077d46ed1
31071997 <zferdous35@gmail.com>
2025-09-17 17:59:02 +0600

    v33

--- COde/SylhetPlace.gd
+++ COde/SylhetPlace.gd
@@ -6,8 +6,22 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		texture = change
 		Score.score += 1
 		label.text = Score.score_label + str(Score.score)
-		
-
-
+@onready var timer: Timer = $Timer
+var timePress = 0
+var time = 0
 func _on_button_pressed() -> void:
-	#pass
+	timePress += 1
+	if(timePress == 1):
+		time = 0
+		timer.start()
+	elif(timePress == 2 and time < 1):
+		print('yes')
+		timer.stop()
+		timePress = 0
+	else:
+		print('no')
+		timePress = 0
+func _on_timer_timeout() -> void:
+	time += 1
+	if(time >= 1):
+		timer.stop() 
--- COde/timer.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-class_name time
-var timer
-func _ready() -> void:
-	timer.get_tree().create_timer(5)
