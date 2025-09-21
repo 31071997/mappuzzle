81c764519cbaefd14235bda22681e1409cbb662d
31071997 <zferdous35@gmail.com>
2025-09-21 19:11:33 +0600

    v34

--- COde/SylhetPlace.gd
+++ COde/SylhetPlace.gd
@@ -1,6 +1,11 @@
 extends Sprite2D
 var change = preload('res://Map/map_project_sylhet.png')
+var path = 'res://Scene/sylhet.tscn'
 @onready var label: Label = $"../Label"
+@onready var button: Button = $Button
+func _process(delta: float) -> void:
+	if(Score.score >= 8):
+		button.disabled = false
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group('Sylhet'):
 		texture = change
@@ -15,11 +20,10 @@ func _on_button_pressed() -> void:
 		time = 0
 		timer.start()
 	elif(timePress == 2 and time < 1):
-		print('yes')
+		get_tree().change_scene_to_file(path)
 		timer.stop()
 		timePress = 0
 	else:
-		print('no')
 		timePress = 0
 func _on_timer_timeout() -> void:
 	time += 1
