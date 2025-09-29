40253d17dca0fe66723c8dbf9d84908b3c48a653
31071997 <zferdous35@gmail.com>
2025-09-29 18:53:47 +0600

    v40

--- COde/pause_menu.gd
+++ COde/pause_menu.gd
@@ -3,9 +3,10 @@ extends Control
 
 func _ready() -> void:
 	var popup = menu_button.get_popup()
-	popup.add_item("Play", 0)
+	popup.add_item("Resume", 0)
 	popup.add_item("Quit", 1)
 	popup.add_item("Restart", 2)
+	popup.add_item("Go to start", 3)
 	popup.id_pressed.connect(_on_menu_item_selected)
 func _on_menu_item_selected(id: int) -> void:
 	match id:
@@ -14,7 +15,11 @@ func _on_menu_item_selected(id: int) -> void:
 		1:
 			get_tree().quit()
 		2:
+			get_tree().paused = false
 			get_tree().reload_current_scene()
 			Score.score = 0
+		3:
+			get_tree().paused = false
+			get_tree().change_scene_to_file('res://Scene/MainMenu.tscn')
 func _on_menu_button_pressed() -> void:
 	get_tree().paused = true
