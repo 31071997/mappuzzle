e28bb20a17550bd0c8615647a1c04175145d0d4c
31071997 <zferdous35@gmail.com>
2025-10-01 01:37:34 +0600

    v40

--- COde/pause_menu.gd
+++ COde/pause_menu.gd
@@ -1,12 +1,12 @@
 extends Control
 @onready var menu_button: MenuButton = $MenuButton
-
+var change = preload("res://icon/letter-x.png")
 func _ready() -> void:
 	var popup = menu_button.get_popup()
 	popup.add_item("Resume", 0)
 	popup.add_item("Quit", 1)
 	popup.add_item("Restart", 2)
-	popup.add_item("Go to start", 3)
+	popup.add_item("Main Menu", 3)
 	popup.id_pressed.connect(_on_menu_item_selected)
 func _on_menu_item_selected(id: int) -> void:
 	match id:
@@ -22,4 +22,5 @@ func _on_menu_item_selected(id: int) -> void:
 			get_tree().paused = false
 			get_tree().change_scene_to_file('res://Scene/MainMenu.tscn')
 func _on_menu_button_pressed() -> void:
+	menu_button.icon = change
 	get_tree().paused = true
